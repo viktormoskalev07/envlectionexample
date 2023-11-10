5d095a64a7529395c45d683ea2307089e8d56788
viktormoskalev07 <80700897+viktormoskalev07@users.noreply.github.com>
2023-11-10 17:26:17 +0200

    test

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main style={{background:process.env.BG}} className={styles.main}>
       <div>
         <h1>{process.env.DB_USER}</h1>
         <h2> {process.env.HELLO_MESSAGE}</h2>
